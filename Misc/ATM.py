68521548ca6e076c1e87514c76dcfcec965ecd25
Harsha Vardhan Gangineni <134440804+HarshaVardhanGangineniUs@users.noreply.github.com>
2023-12-25 12:40:17 -0500

    basic operations for ATM Code is completed

--- Misc/ATM.py
+++ Misc/ATM.py
@@ -1,13 +1,16 @@
 import os
+import time
+
+
 def bank():
     print("Hello ")
     acc_holder_name = input("Please enter your name:")
     acc_holder_balance = int(input("Enter your initial balance "))
     banking_option_selection = 0
     while banking_option_selection != 4:
-        os.system('cls')
-        print(f"Hello {name}", end="\n")
-        print("1. Credit" , end="\n")
+        os.system('clear')
+        print(f"Hello {acc_holder_name}", end="\n")
+        print("1. Credit", end="\n")
         print("2. Debit", end="\n")
         print("3. Show Balance", end="\n")
         print("4. Exit.", end="\n")
@@ -20,14 +23,13 @@ def bank():
             acc_holder_balance -= amount_to_be_debited
         elif banking_option_selection == 3:
             print(f"Total balance in your account is {acc_holder_balance}")
-        elif    banking_option_selection == 4:
+            time.sleep(5)
+        elif banking_option_selection == 4:
             exit()
-
-
-
-
-
+        else:
+            print("Please select a correct option")
+            time.sleep(5)
 
 
 if __name__ == "__main__":
-    print("Hi, Hello world!!")
+    bank()
